4e507bd6cf311c6d5841727b0aa7cca03f876f93
Marcos Allan <marcosallanm1to@gmail.com>
2022-10-31 18:53:38 -0300

    Máscara de preço

--- js/main.js
+++ js/main.js
@@ -18,4 +18,32 @@ export const validaForm = () => {
   });
 };
 
+export const validaAdicionarProduto = () => {
+  const botaoCadastro = document.querySelector("[data-botao='salvar']");
+  const inputsCadastro = document.querySelectorAll("[data-cadastro]");
+
+  botaoCadastro.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    inputsCadastro.forEach((input) => {
+      valida(input, input.dataset.cadastro);
+    });
+  });
+
+  inputsCadastro.forEach((input) => {
+    if (input.dataset.cadastro == "preco") {
+      SimpleMaskMoney.setMask(input, {
+        prefix: "R$ ",
+        fixed: true,
+        fractionDigits: 2,
+        decimalSeparator: ",",
+        thousandsSeparator: ".",
+        cursor: "end",
+      });
+    }
+    input.addEventListener("blur", () => {
+      valida(input, input.dataset.cadastro);
+    });
+  });
+};
+
 validaForm();
